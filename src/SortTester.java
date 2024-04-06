858c29acd6e64d87c04bccedab0bb99415aef327
Shibam <mukhopad2@grinnell.edu>
2024-04-05 22:06:54 -0500

    fixed all tests

--- src/SortTester.java
+++ src/SortTester.java
@@ -32,20 +32,73 @@ public class SortTester {
 
   @Test
   public void orderedStringTest() {
-    sorter = new MergeSort();
+    sorter = new InsertionSort();
     String[] original = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
     String[] expected = original.clone();
     sorter.sort(original, (x, y) -> x.compareTo(y));
-    assertArrayEquals(original, expected);
+    assertArrayEquals(expected, original);
   } // orderedStringTest
 
   @Test
   public void reverseOrderedStringTest() {
     sorter = new MukhopadhyayShibamSort();
-    String[] original = { "foxtrot", "delta", "charlie", "bravo", "alpha" };
+    String[] original = { "delta", "charlie", "bravo", "foxtrot", "alpha" };
     String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
     sorter.sort(original, (x, y) -> x.compareTo(y));
     assertArrayEquals(expected, original);
-  } // orderedStringTest
+  } // reverseOrderedStringTest
 
+
+  @Test
+  public void randomlyOrderedStringTest() {
+    sorter = new MergeSort();
+    String[] original = { "bravo", "delta", "foxtrot", "charlie", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // randomlyOrderedStringTest
+
+  @Test
+  public void multipleSameStringTest() {
+    sorter = new Quicksort();
+    String[] original = { "foxtrot", "alpha", "bravo", "alpha", "delta", "charlie" };
+    String[] expected = { "alpha", "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // multipleSameStringTest
+
+  @Test
+  public void orderedIntTest() {
+    sorter = new InsertionSort();
+    Integer[] original = {1, 54, 58, 62, 90};
+    Integer[] expected = {1, 54, 58, 62, 90};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // orderedIntTest
+
+  @Test
+  public void reverseOrderedIntTest() {
+    sorter = new MukhopadhyayShibamSort();
+    Integer[] original = {21, 17, 13, 1};
+    Integer[] expected = {1, 13, 17, 21};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // reverseOrderedIntTest
+
+  @Test
+  public void randomlyOrderedIntTest() {
+    sorter = new MergeSort();
+    Integer[] original = {73, 54, 67, 102, 13, 9, 3};
+    Integer[] expected = {3, 9, 13, 54, 67, 73, 102};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // randomlyOrderedIntTest
+  @Test
+  public void multipleSameIntTest() {
+    sorter = new Quicksort();
+    Integer[] original = {42, 79, 112, 29, 79, 42, 112};
+    Integer[] expected = {29, 42, 42, 79, 79, 112, 112};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(expected, original);
+  } // mulitpleSameIntTest
 } // class SortTester
